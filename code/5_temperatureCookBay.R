6985d22d40958a796f454e506483f0ecef316e66
mikelitzow <mike.litzow@noaa.gov>
2023-01-03 15:00:56 -0900

    Cleaned up, fit GAM to predict max temp by year

--- code/5_temperatureCookBay.R
+++ code/5_temperatureCookBay.R
@@ -6,15 +6,21 @@ library(lubridate)
 # Read in the data and rename any columns that need renaming
 cooktemp <- read_csv("data/DailyTempAvg1.csv")
 head(cooktemp)
-cooktemp <- rename(cooktemp, "Avg_Temp" = "Avg Temp")
+
+cooktemp <- cooktemp %>%
+  rename("Avg_Temp" = "Avg Temp")
+
 head(cooktemp)
 tail(cooktemp)
+
 ##can see that last rows are all NA. Must remove these last 3 rows
 cooktemp <-filter(cooktemp, !is.na(year))
+
+
 tail(cooktemp) #you can see it worked 
 
 #need to remove years 2021
-cooktemp<-filter(cooktemp,year=="2018"|year=="2019" | year == "2020")
+cooktemp<-filter(cooktemp,year=="2018"|year=="2019" | year == "2020") # year %in% c("2018", "2019", "2020")
 distinct(cooktemp, year)
 
 
@@ -23,14 +29,42 @@ cooktemp <- cooktemp %>%
   mutate(year_fac = as.factor(year))
 
 cooktemp <- cooktemp %>%
-  mutate(Avg_Temp_C = as.integer(Avg_Temp))
+  mutate(Avg_Temp = as.numeric(Avg_Temp))
+
 str(cooktemp)
-ggplot(cooktemp, aes(J_date, Avg_Temp_C, color = year_fac)) +
-  geom_point() +
+
+ggplot(cooktemp, aes(J_date, Avg_Temp, color = year_fac)) +
+  geom_point(alpha = 0.2) +
   theme_bw() +
   geom_smooth(method = "gam", formula = y ~ s(x, k = 8), se = F)
 
 ggsave("./figs/Cook_Bay_Temp1.png", width = 6, height = 4, units = 'in')
+
+# fit gam to predict maximum, etc. 
+drop <- is.na(cooktemp$Avg_Temp)
+cooktemp <- cooktemp[!drop,]
+
+# check
+sum(is.na(cooktemp$Avg_Temp))
+
+mod_2018 <- mgcv::gam(Avg_Temp ~ s(J_date, k = 8), data = filter(cooktemp, year_fac == 2018))
+
+plot(mod_2018, resid = T, pch = 19, se = F)
+
+max(predict(mod_2018, se.fit = F))
+## 2019
+mod_2019 <- mgcv::gam(Avg_Temp ~ s(J_date, k = 8), data = filter(cooktemp, year_fac == 2019))
+
+plot(mod_2019, resid = T, pch = 19, se = F)
+
+max(predict(mod_2019, se.fit = F))
+
+## 2020
+mod_2020 <- mgcv::gam(Avg_Temp ~ s(J_date, k = 8), data = filter(cooktemp, year_fac == 2020))
+
+plot(mod_2020, resid = T, pch = 19, se = F)
+
+max(predict(mod_2020, se.fit = F))
 
 ##Mike make plot better by only showing curve and not datapoints?
 ##Mike test if years different in cooktemp?
